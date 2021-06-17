26718f00f1a44a271ce8b851e7958d6cba4008f7
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-17 16:37:16 +0530

    master

--- commands.sh
+++ commands.sh
@@ -3,3 +3,4 @@ git checkout branch //switch between branches
 git push --set-upstream origin branch
 git branch -d master //Delete the branch
 git push origin --delete test
+git branch -l //get the list of branches
