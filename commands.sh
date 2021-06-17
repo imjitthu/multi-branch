1bc3561d35a6a977f42c6bcee0d0c94955c7b2ff
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-17 16:40:02 +0530

    master

--- commands.sh
+++ commands.sh
@@ -4,3 +4,6 @@ git push --set-upstream origin branch
 git branch -d master //Delete the branch
 git push origin --delete test
 git branch -l //get the list of branches
+git tag test //add tag to git
+git push --tags //push tags to the origin
+git tag -l //get the all tags list
