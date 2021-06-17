af9f8829796d9ee7e9e4cc2f648b7571a002e85a
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-17 16:36:20 +0530

    master

--- commands.sh
+++ commands.sh
@@ -1,4 +1,5 @@
-git checkout -b branch
+git checkout -b branch //create new branch and switch to it
+git checkout branch //switch between branches
 git push --set-upstream origin branch
 git branch -d master //Delete the branch
 git push origin --delete test
