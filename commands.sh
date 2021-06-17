524df1433ee3cbb58fce7c748dbcdeb277afe2a3
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-17 17:24:05 +0530

    multi branch

--- commands.sh
+++ commands.sh
@@ -5,7 +5,12 @@ git branch -d master //Delete the branch
 git push origin --delete test
 git branch -l //get the list of branches
 git tag test //add tag to git
+it tag -a test.release -m "test verison release" //annotated tag
+git push origin tag //push tag to remote repository
 git push --tags //push tags to the origin
 git tag -l //get the all tags list
+git tag --delete tag //delete the local tag
+git push origin -d test
+git show tag
 
 only executes when changes happened in SCM
